07edaa9fcc0cbde9361961f003d2a7775cd65552
asuparabaco0 <rina2000428@yahoo.co.jp>
2021-07-08 17:41:46 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useCallback, useState, useEffect } from 'react';
+import Navigation from './components/Navigation';
+import Field from './components/Field';
+import Button from './components/Button';
+import ManipulationPanel from './components/ManipulationPanel';
+import {initFields, getFoodPosition} from './utils/index'
+const initialPosition = {x: 17, y: 17}
+const initialValues =initFields(35, initialPosition)
+const defaultInterval = 100
+const defaultDifficulty = 3
+const Difficulty = [1000, 500, 100, 50, 10]
+
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover'
+});
+const Direction = Object.freeze({
+  up: 'up',
+  right: 'right',
+  left: 'left',
+  down: 'down'
+})
+ const OppositeDirection = Object.freeze({
+   up: 'down',
+   right: 'left',
+   left: 'right',
+   down: 'up'
+ })
+
+let timer = undefined;
+const unsubscribe = () => {
+  if(!timer){
+    return
+  }
+  clearInterval(timer)
+}
+
+const isCollision = (fieldSize, position) => {
+  if(position.y < 0 || position.x < 0){
+    return true;
+  }
+  if(position.y > fieldSize - 1 || position.x > fieldSize - 1){
+    return true;
+  }
+
+  return false;
+}
+
+const isEatingMyself = (fields, position) => {
+  return fields[position.y][position.x] === 'snake'
+}
 
 function App() {
+  const[fields, setFields] = useState(initialValues)
+  const [position, setPosition] = useState()
+  const [body,setBody] = useState([])
+  const [status, setStatus] = useState(GameStatus.init)
+  const [direction,setDirection] = useState(Direction.up)
+  const [difficulty, setDifficulty] = useState(defaultDifficulty)
+  const [tick, setTick]=useState(0)
+  useEffect(()=>{
+    setBody([initialPosition])
+    //ゲームの中の時間を管理する
+    const interval = Difficulty[difficulty - 1]
+    timer = setInterval(() => {
+      setTick(tick => tick + 1)
+    }, interval)
+    return unsubscribe
+  }, [difficulty])
+
+    useEffect(()=>{
+      if(body.length === 0 || status !== GameStatus.playing){
+        return
+      }
+      const canContinue = handleMoving()
+      if(!canContinue){
+        unsubscribe()
+        setStatus(GameStatus.gameover)
+      }
+    },[tick])
+const onStart = () => setStatus(GameStatus.playing)
+const onStop = () => setStatus(GameStatus.suspended)
+const onRestart = () => {
+  timer = setInterval(() => {
+    setTick(tick => tick + 1)
+  }, defaultInterval)
+  setDirection(Direction.up)
+  setStatus(GameStatus.init)
+  setBody([initialPosition])
+  setFields(initFields(35, initialPosition))
+}
+const onChangeDirection = useCallback((newDirection) => {
+  if(status !== GameStatus.playing){
+    return Direction
+  }
+if (OppositeDirection[direction] === newDirection){
+return
+}
+setDirection(newDirection)
+},[direction , status])
+const onChangeDifficulty = useCallback((difficulty) => {
+if(status !== GameStatus.init){
+  return
+}
+if(difficulty < 1 || difficulty > Difficulty.length){
+  return
+}
+setDifficulty(difficulty)
+},[status,difficulty])
+
+useEffect(() => {
+  const handleKeyDown = (e) => {
+    const newDirection = DirectionKeyCodeMap[e.keyCode]
+    if(!newDirection){
+      return
+    }
+    onChangeDirection(newDirection)
+  }
+  document.addEventListener('keydown', handleKeyDown)
+  return () => document.removeEventListener('keydown', handleKeyDown)
+},[onChangeDirection])
+
+const Delta = Object.freeze({
+  up: {x:0,y:-1},
+  right: {x:1,y:0},
+  left: {x:-1,y:0},
+  down: {x:0,y:1}
+})
+const DirectionKeyCodeMap = Object.freeze({
+  37: Direction.left,
+  38: Direction.up,
+  39: Direction.right,
+  40: Direction.down,
+})
+// const handleKeyDown = (e) =>{
+//   const newDirection = DirectionKeyCodeMap[e.keyCode];
+//   if(!newDirection) {
+//     return;
+//   }
+//   onChangeDirection();
+// // }
+// document.addEventListener('keyDown',handleKeyDown);
+const handleMoving = () => {
+  const { x, y } = body[0]
+  const delta = Delta[direction]
+  const newPosition = { 
+    x: x + delta.x,
+    y: y + delta.y 
+  }
+      if (isCollision(fields.length, newPosition) || isEatingMyself(fields, newPosition)) {
+          return false
+      }
+  const newBody = [...body]
+  if (fields[newPosition.y][newPosition.x] !== 'food') {
+    const removingTrack = newBody.pop()
+    fields[removingTrack.y][removingTrack.x] = ''
+  } else {
+    const food = getFoodPosition(fields.length,[...newBody, newPosition])
+    fields[food.y][food.x] = 'food'
+  }
+  fields[newPosition.y][newPosition.x] = 'snake'
+  newBody.unshift(newPosition)
+
+  setBody(newBody)
+  setFields(fields)
+  return true
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <div className='title-container'>
+          <h1 className='title'>🐍Snake Game🐍</h1>
+        </div>
+          <Navigation 
+          length={body.length} 
+          difficulty={difficulty}
+          onChangeDifficulty={onChangeDifficulty}/>
       </header>
+      <main className='main'>
+        <Field fields={initialValues}/>
+      </main>
+      <footer className='footer'>
+        <Button status={status} onStart={onStart} onStop={onStop}　onRestart={onRestart}/>
+        <ManipulationPanel onChange={onChangeDirection}/>
+      </footer>
     </div>
   );
 }
